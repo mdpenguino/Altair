3ee8bd673a790b5f20dec3c35771f8c31acc3b9e
mdpenguino <mdpenguino447@gmail.com>
2018-05-27 18:34:51 +0100

    edits to grid and content loading

--- src/js/grid.js
+++ src/js/grid.js
@@ -1,11 +1,13 @@
 var emptyCells, i;
 
-$('.grid.with-empty-cells').each(function() {
+$(".test.with-empty-cells").each(function() {
   emptyCells = [];
-  for (i = 0; i < $(this).find('.cell').length; i++) {
-    emptyCells.push($('<ul>', {
-      class: 'cell is-empty'
-    }));
+  for (i = 0; i < $(this).find(".cell").length; i++) {
+    emptyCells.push(
+      $("<ul>", {
+        class: "cell is-empty"
+      })
+    );
   }
   $(this).append(emptyCells);
 });
