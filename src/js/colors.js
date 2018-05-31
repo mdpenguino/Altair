10a556c819e1264bcadc34193be03d32afc3e7e7
mdpenguino <mdpenguino447@gmail.com>
2018-05-31 11:42:55 +0100

    Code Changes Added comments to colors.js Removed redundant code from stylesheet.css

--- src/js/colors.js
+++ src/js/colors.js
@@ -1,9 +1,11 @@
+//Colour switching code for header
 function changeColor(){
   var newColor = document.getElementById('background').value;
 	document.getElementById('header').style.background = newColor;
   document.getElementById('frame-bar').style.background = newColor;
 }
 
+//Frameless window buttons and actions
 (function () {
 
       const remote = require('electron').remote;
