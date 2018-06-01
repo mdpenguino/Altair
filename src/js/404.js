fca05888ad8b56a445b85f7dadf0f36f3d1a502d
David Doyle <loldavid6@gmail.com>
2018-06-01 23:58:08 +0100

    gg
    gg

---
+++ src/js/404.js
@@ -0,0 +1,10 @@
+var random_images_array = ["a.bmp",
+"b.bmp"
+];
+
+path = '../src/assets/404';
+var num = Math.floor( Math.random() * random_images_array.length );
+var img = random_images_array[ num ];
+var imgStr = '<img src="' + path + img + '">';
+
+document.write(imgStr); document.close();
