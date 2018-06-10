4fbde207d2b7f10c7046375cbb9e125598e330fa
mdpenguino <mdpenguino447@gmail.com>
2018-06-10 17:23:56 +0100

    Modal Popup Box

--- src/js/colors.js
+++ src/js/colors.js
@@ -12,3 +12,30 @@ window.addEventListener("load", showPage, false);
 function showPage() {
   document.documentElement.className = document.documentElement.className.replace("preloader","");
 }
+window.onload = function(){
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var a = document.getElementById("modallink");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+a.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+}
