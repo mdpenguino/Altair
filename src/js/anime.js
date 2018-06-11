5096cb95043e2362170ea4ba56222cf79209cbb4
David Doyle <loldavid6@gmail.com>
2018-06-11 18:45:07 +0100

    yee

--- src/js/anime.js
+++ src/js/anime.js
@@ -1,11 +1,18 @@
-<li>
-  <div class="card">
-    <a href="">
-    <img class="cardimg" draggable="false" src="https://myanimelist.cdn-dena.com/images/anime/1614/90408.jpg" alt="anime cover">
-    <div class="container">
-      <h4><b>Darling in the FranXX</b></h4>
-      <p class="cardtext">18/24</p>
-    </div>
-    </a>
-  </div>
-</li>
+//<li>
+//  <div class="card">
+//    <a href="">
+//    <img class="cardimg" draggable="false" src="https://myanimelist.cdn-dena.com/images/anime/1614/90408.jpg" alt="anime cover">
+//    <div class="container">
+//      <h4><b>Darling in the FranXX</b></h4>
+//      <p class="cardtext">18/24</p>
+//    </div>
+//    </a>
+//  </div>
+//</li>
+
+
+
+//temp storage for data
+var 35849 = {title:"Darling in the FranXX", image_url:"https://myanimelist.cdn-dena.com/images/anime/1614/90408.jpg"};
+var 36475 = {title:"Sword Art Online Alternative: Gun Gale Online", image_url:"https://myanimelist.cdn-dena.com/images/anime/1114/91791.jpg"};
+var 30484 = {title:"Steins;Gate 0", image_url:"https://myanimelist.cdn-dena.com/images/anime/1031/90444.jpg"};
