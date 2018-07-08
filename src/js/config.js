1e103912fd32b3e892f70d9491d8e67036af6c1c
David Doyle <loldavid6@gmail.com>
2018-07-08 16:55:34 +0100

    ggnore

---
+++ src/js/config.js
@@ -0,0 +1,9 @@
+const electron = require('electron');
+const path = require('path');
+const fs = require('fs');
+
+var appdata = app.getPath
+var configjsonpath = appdata + "config" + ".json"
+
+console.log appdata
+console.log configjsonpath
