7d87e515e08c1759928f6abd5a3ec4723d4a0c18
mdpenguino <mdpenguino447@gmail.com>
2018-07-13 18:26:40 +0100

    Styling and Close Button

--- src/js/about.js
+++ src/js/about.js
@@ -5,3 +5,18 @@ $(document).on('click', 'a[href^="http"]', function(event) {
     event.preventDefault();
     shell.openExternal(this.href);
 });
+
+(function () {
+      const remote = require('electron').remote;
+      function init() {
+        document.getElementById("aboutClose").addEventListener("click", function (e) {
+          const window = remote.getCurrentWindow();
+          window.close();
+        });
+      };
+      document.onreadystatechange = function () {
+        if (document.readyState == "complete") {
+          init();
+        }
+      };
+})();
